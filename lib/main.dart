c7a4caa37bf1a54e84a42ac2793718de728a79d7
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-12-21 16:32:05 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'dart:html';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Home()));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello"),
+        centerTitle: true,
+        backgroundColor: Colors.red[300],
+      ),
+      body: Center(
+        child: Image.asset("../assets/space-1.jpg"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text("Yolo"),
+        onPressed: () {},
+      ),
+    );
+  }
+}
