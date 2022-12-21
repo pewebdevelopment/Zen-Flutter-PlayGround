cbdcf1d15ef961321c6366a76dfcefd20ca026a4
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-12-21 18:05:46 +0530

    Inside Container Button Putted

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,22 @@ class Home extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.red[300],
         ),
-        body: Padding(
-          padding: EdgeInsets.all(90.0),
-          child: Text('Hello'),
+        body: Row(
+          children: <Widget>[
+            Text('Hello, World'),
+            TextButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.amber),
+              ),
+              onPressed: () {},
+              child: Text('click me'),
+            ),
+            Container(
+              color: Colors.cyan,
+              padding: EdgeInsets.all(30.0),
+              child: Text('inside container'),
+            )
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           child: Text("Yolo"),
