f8d328e6f53249dbbd88b40ec1ad6db1d71cc061
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-12-21 17:42:08 +0530

    margin placed

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class Home extends StatelessWidget {
         ),
         body: Container(
             padding: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
+            margin: EdgeInsets.all(30.0),
             color: Colors.grey[400],
             child: Text('Hello')),
         floatingActionButton: FloatingActionButton(
