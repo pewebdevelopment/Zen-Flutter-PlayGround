3fb5a30d26d50ecd0bd9ef864551a04aeac68a49
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-12-21 17:20:15 +0530

    FlotingActionButton Error Placed

--- lib/main.dart
+++ lib/main.dart
@@ -9,25 +9,25 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello"),
-        centerTitle: true,
-        backgroundColor: Colors.red[300],
-      ),
-      body: Center(
-        child: TextButton(
-          style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.all<Color>(Colors.blue)),
-          onPressed: () {
-            print('you clicked me');
-          },
-          child: Text('click me'),
+        appBar: AppBar(
+          title: Text("Hello"),
+          centerTitle: true,
+          backgroundColor: Colors.red[300],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Text("Yolo"),
-        onPressed: () {},
-      ),
-    );
+        body: Center(
+          child: ElevatedButton.icon(
+              onPressed: () {
+                print("You pressed Icon Elevated Button");
+              },
+              icon: Icon(Icons.mail), //icon data for elevated button
+              label: Text("mail me"), //label text
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.amber //elevated btton background color
+                  )),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text("Yolo"),
+          onPressed: () {},
+        ));
   }
 }
